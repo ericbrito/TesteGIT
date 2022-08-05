6661b753e550f60b1533d5d1e2cb1981ddb0f72c
Eric Brito <ealvesbrito@ebay.com>
2022-08-04 22:27:33 -0300

    Initial Commit

---
+++ TesteGIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TesteGIT
+//
+//  Created by Alves Brito, Eric(AWF) on 04/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
